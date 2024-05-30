e5a8e803010035053d772e42c143c316e951188d
spottsmorpheus <spotts@morpheusdata.com>
2024-05-30 20:41:37 +0100

    Test domain join

--- groovy/testDomainJoin.groovy
+++ groovy/testDomainJoin.groovy
@@ -8,10 +8,12 @@ import static groovy.json.JsonOutput.*
 
 //Def and variables needed by the class here
 
-//Call constructor
+//Call constructor - need to tweak these
+def domainId=21
+def serverId=15
 def classTool = new MyWindowsOsTool(userDisplayName,customOptions)
 //Call Member
-classTool.runIt()
+classTool.runIt(domainId,serverId)
 
 @Slf4j
 class MyWindowsOsTool {
@@ -25,7 +27,6 @@ class MyWindowsOsTool {
 
 
     public MyWindowsOsTool(name,customOptions,intId=0,zoneId=0,siteId=0) {
-        
         this.grailsApplication = Holders.grailsApplication
         this.mainContext = Holders.grailsApplication.mainContext
     	this.name = name
@@ -38,12 +39,24 @@ class MyWindowsOsTool {
 
     }
     
-
-    public runIt() {
-        
-        log.info("runIt method")
-        this.windowsOsService.
-
-    }
-    
+    public runIt(domainId,serverId) {
+        def opts = [networkConfig : [networkDomain : [ : ]]]
+        log.info("runIt method: loading domain ${domainId}")
+        NetworkDomain.withNewSession {session ->
+           NetworkDomain domain = NetworkDomain.get(domainId)
+           ComputeServer server = ComputeServer.get(serverId)
+           log.info("MyWindowsOsTool - domain - ${domain.getProperties()}")
+           opts.customized = true
+           opts.unattendCustomized = true
+           opts.desiredHostName = null
+           opts.networkConfig.networkDomain.name = domain.name
+           opts.networkConfig.networkDomain.domainUsername = domain.domainUsername
+           opts.networkConfig.networkDomain.domainPassword = domain.domainPassword
+           opts.networkConfig.networkDomain.dcServer = domain.dcServer
+           opts.networkConfig.networkDomain.ouPath = domain.ouPath
+           log.info("MyWindowsOsTool - opts - ${opts}")
+           def cmd = windowsOsService.buildJoinDomainScript(server,opts)
+           log.info("MyWindowsOsTool - cmd ${cmd}")
+        }
+    } 
 }
