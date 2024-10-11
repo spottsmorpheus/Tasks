709ab4e3882ac7455df4aed0df1aa11b5c38b63b
spottsmorpheus <spotts@morpheusdata.com>
2024-10-11 08:56:28 +0100

    Update CodeGrep to take in -FileType and remove outHtml

--- pwsh/codeGrep.ps1
+++ pwsh/codeGrep.ps1
@@ -2,11 +2,12 @@ Function Grep-Code {
     [CmdletBinding()]
     param (
         [String]$Pattern,
-        [String]$Path
+        [String]$Path,
+        [String]$FileType="*.groovy"
     )
 
 
-    $GroovyFiles = Get-ChildItem -Path $Path -Include *.groovy -Recurse
+    $GroovyFiles = Get-ChildItem -Path $Path -Include $FileType -Recurse
     $MatchedItems = $GroovyFiles | Select-String -Pattern $Pattern
     return $MatchedItems | Select-Object -Property Filename,LineNumber,@{N="MatchedLine";E={$_.Line.trim()}}
 }
